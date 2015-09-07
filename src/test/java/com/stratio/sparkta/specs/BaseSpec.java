a3094c3c03893fa2f50f1c36a4505be5785856fe
antonioalf <aalfonso@stratio.com>
2015-09-07 18:15:30 +0200

    previous value management clean

--- src/test/java/com/stratio/sparkta/specs/BaseSpec.java
+++ src/test/java/com/stratio/sparkta/specs/BaseSpec.java
@@ -3,11 +3,5 @@ package com.stratio.sparkta.specs;
 import com.stratio.specs.BaseGSpec;
 
 public class BaseSpec extends BaseGSpec{
-     
-//    protected Common commonspec;
-//    
-//    public Common getCommonSpec() {
-//	return this.commonspec;
-//    }
-    
+       
 }
--- src/test/java/com/stratio/sparkta/specs/Common.java
+++ src/test/java/com/stratio/sparkta/specs/Common.java
@@ -8,28 +8,9 @@ public class Common extends CommonG {
 
     public static String previousFragmentID = "";
     
-//    public static void setpreviousFragmentID(String value) {
-//	//this.getLogger().info("SETTING VALUE TO: {}", value);
-//	setPreviousFragmentID(value);
-//    }
-//    
-//    public String getpreviousFragmentID() {
-//	this.getLogger().info("GETTING VALUE");
-//	return this.getPreviousFragmentID();
-//    }
-    
     public void setPreviousElement(String value) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
 	this.getLogger().info("SETTING VALUE TO: {}", value);
-	    //setPreviousFragmentID(value);
 	previousFragmentID = value;
     }
-
-//    public static String getPreviousFragmentID() {
-//	return previousFragmentID;
-//    }
-//
-//    public static void setPreviousFragmentID(String previousFragmentID) {
-//	Common.previousFragmentID = previousFragmentID;
-//    }
     
 }

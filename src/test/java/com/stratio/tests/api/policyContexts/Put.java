0e0165173a4a014fcbc51f54004ac8e4c4d74cdb
antonioalf <aalfonso@stratio.com>
2015-09-18 10:57:26 +0200

    small fix to correctly point to feature

--- src/test/java/com/stratio/tests/api/policyContexts/Put.java
+++ src/test/java/com/stratio/tests/api/policyContexts/Put.java
@@ -7,7 +7,7 @@ import com.stratio.tests.utils.BaseTest;
 
 import cucumber.api.CucumberOptions;
 
-@CucumberOptions(features = { "src/test/resources/features/api/policycContexts/putPolicyContexts.feature" })
+@CucumberOptions(features = { "src/test/resources/features/api/policyContexts/putPolicyContexts.feature" })
 public class Put extends BaseTest {
 
     public Put() {
